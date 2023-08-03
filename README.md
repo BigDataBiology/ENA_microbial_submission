efc0f81f56c22403111c715ac01bffe19ffb41b9
rfm-targa <rmamede@medicina.ulisboa.pt>
2023-08-03 17:28:28 +0100

    Added scripts and example metadata.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# ENA_microbial_submission
-Set of scripts and instructions to upload sequencing reads to ENA.
+# Submission of microbial sample data to the European Nucleotide Archive (ENA)
+
+We suggest that you start by reading the General Guide On ENA Data Submission https://ena-docs.readthedocs.io/en/latest/submit/general-guide.html. This repo contains scripts and instructions to submit raw reads programmatically. The only step that is performed interactively is the Study Registration. You need to have Webin submission account to be able to submit data to the ENA (informationa bout creating an account here https://ena-docs.readthedocs.io/en/latest/submit/general-guide/registration.html#register-a-submission-account)
+
+
+Start by registering a Study. You can find instructions about this step here https://ena-docs.readthedocs.io/en/latest/submit/study.html and here https://ena-docs.readthedocs.io/en/latest/submit/study/interactive.html. We find it easier to register a new Study interactively, especially when the raw reads we want to submit will become associated to a single Study, but you can find instructions to do this programatically here https://ena-docs.readthedocs.io/en/latest/submit/study/programmatic.html.
+
+The next step is to register samples. You can find information about this step here https://ena-docs.readthedocs.io/en/latest/submit/samples.html and here https://ena-docs.readthedocs.io/en/latest/submit/samples/programmatic.html. We will use the ERC000028 checklist (ENA prokaryotic pathogen minimal sample checklist, available here https://www.ebi.ac.uk/ena/browser/view/ERC000028). We will be constructing the XML based on the fields in the checklist (we will not modify the checklist file for submission). This repo includes a TSV file, test_metadata.tsv, with example metadata necessary to register samples in the ENA. To create the XML file to register your samples you should run the following command:
+
+python ena_samples_xml.py -i test_sample_metadata.tsv -o sample.xml -c ERC000028
+
+This will create a XML file with the data to register the samples and the accompanying submission XML file, submission.xml.
+To submit the XML files and register your samples, you can run the following command:
+
+curl -u username:password -F "SUBMISSION=@submission.xml" -F "SAMPLE=@sample.xml" "https://www.ebi.ac.uk/ena/submit/drop-box/submit/"
+
+This information is also availabel here https://ena-docs.readthedocs.io/en/latest/submit/samples/programmatic.html#submit-the-xmls-using-curl. Please provide your Webin submission account credentials using the username and password. You should test your submissions before submitting the data to the production service (more information here https://ena-docs.readthedocs.io/en/latest/submit/samples/programmatic.html#test-and-production-services).
+
+To upload read files:
+
+1. Open a terminal and type lftp webin2.ebi.ac.uk -u Webin-xxxxx, filling in your Webin username (must have lftp installed).
+2. Enter your password when prompted.
+3. Type ls command to check the content of your drop box.
+4. Use mput <filename> command to upload files.
+5. Use bye command to exit the ftp client.
+
+Register Runs and Experiments:
+
+You should prepare the FASTQ files for upload. FASTQ files must be compressed before upload (with gzip or bzip2, more information here https://ena-docs.readthedocs.io/en/latest/submit/fileprep/preparation.html#preparing-a-file-for-upload).
+You also need to create a TSV file with the data necessary to register the runs and experiments. To create the XML files to register the Runs and Experiments, run the following command:
+
+python ena_reads.xml.py -i test_run_experiment_metadata.tsv -o datasetID --study studyID --reads readsDir
+
+curl -u username:password -F "SUBMISSION=@submission.xml" -F "EXPERIMENT=@experiment.xml" -F "RUN=@run.xml" "https://www.ebi.ac.uk/ena/submit/drop-box/submit/"
---
+++ ena_samples_xml.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This script creates a XML file to register a list of samples in the ENA.
+"""
+
+
+import os
+import argparse
+import xml.etree.ElementTree as ET
+
+import pandas as pd
+
+
+def create_submission_xml(output_directory):
+    """Create the submission XML."""
+
+    tree = ET.ElementTree('tree')
+    # Create parent tag
+    parent_tag = ET.Element('SUBMISSION')
+    # Add ACTIONS tag
+    actions_tag = ET.SubElement(parent_tag, 'ACTIONS')
+    # Add ACTION tag
+    action_tag = ET.SubElement(actions_tag, 'ACTION')
+    # Add ADD tag
+    add_tag = ET.SubElement(action_tag, 'ADD')
+
+    output_file = os.path.join(output_directory, 'submission.xml')
+
+    tree._setroot(parent_tag)
+    tree.write(output_file, encoding='UTF-8', xml_declaration=True)
+
+
+def main(input_file, output_file, checklist):
+    """Create the XML file with the sample data to submit."""
+
+    # Read input file with sample metadata
+    data_table = pd.read_csv(input_file, sep='\t')
+
+    # Get headers to add as sample attributes
+    attributes = list(data_table.columns)
+    attributes = [a
+                  for a in attributes
+                  if a not in ['alias', 'title', 'taxon_id', 'center_name']]
+
+    tree = ET.ElementTree('tree')
+
+    # Create parent tag
+    parent_tag = ET.Element('SAMPLE_SET')
+
+    # Add tags for each sample
+    for i in range(len(data_table)):
+        current_sample = data_table.iloc[i]
+        # Add SAMPLE tag
+        sample_tag = ET.SubElement(parent_tag, 'SAMPLE')
+        # Add alias attribute to SAMPLE tag
+        sample_tag.set('alias', current_sample['alias'])
+        # Add center_name attribute to SAMPLE tag
+        sample_tag.set('center_name', current_sample['center_name'])
+        # Add TITLE tag
+        title_tag = ET.SubElement(sample_tag, 'TITLE')
+        title_tag.text = current_sample['title']
+        # Add SAMPLE_NAME tag
+        sample_name_tag = ET.SubElement(sample_tag, 'SAMPLE_NAME')
+        # Add TAXON_ID tag
+        taxonid_tag = ET.SubElement(sample_name_tag, 'TAXON_ID')
+        taxonid_tag.text = str(current_sample['taxon_id'])
+
+        # Create SAMPLE_ATTRIBUTES tags and add each attribute value
+        sample_attributes_tag = ET.SubElement(sample_tag, 'SAMPLE_ATTRIBUTES')
+        for a in attributes:
+            current_attribute = a
+            current_value = current_sample[current_attribute]
+            current_attribute_tag = ET.SubElement(sample_attributes_tag, 'SAMPLE_ATTRIBUTE')
+            # Add TAG tag
+            current_tag_tag = ET.SubElement(current_attribute_tag, 'TAG')
+            current_tag_tag.text = current_attribute.lower()
+            # Add VALUE tag
+            current_value_tag = ET.SubElement(current_attribute_tag, 'VALUE')
+            current_value_tag.text = str(current_value)
+
+        # Add ENA checklist identifier to each sample
+        if checklist is not None:
+            checklist_attribute_tag = ET.SubElement(sample_attributes_tag, 'SAMPLE_ATTRIBUTE')
+            # Add TAG tag
+            checklist_attribute_tag_tag = ET.SubElement(checklist_attribute_tag, 'TAG')
+            checklist_attribute_tag_tag.text = 'ENA-CHECKLIST'
+            # Add VALUE tag
+            checklist_attribute_tag_value = ET.SubElement(checklist_attribute_tag, 'VALUE')
+            checklist_attribute_tag_value.text = checklist
+
+    tree._setroot(parent_tag)
+    # Write XML file with sample data
+    tree.write(output_file, encoding='UTF-8', xml_declaration=True)
+
+    # Write the submission XML
+    output_directory = os.path.dirname(output_file)
+    create_submission_xml(output_directory)
+
+
+def parse_arguments():
+
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('-i', '--input-file', type=str, required=True,
+                        dest='input_file',
+                        help='Path to the input TSV file with the metadata that should be included in the XML.')
+
+    parser.add_argument('-o', '--output-file', type=str, required=True,
+                        dest='output_file',
+                        help='Path to to the output XML file (please include the .xml extension.)')
+
+    parser.add_argument('-c', '--checklist', type=str, required=True,
+                        dest='checklist',
+                        help='Identifier of the checklist to use (e.g. ERC000028).')
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == '__main__':
+
+    args = parse_arguments()
+    main(**vars(args))
